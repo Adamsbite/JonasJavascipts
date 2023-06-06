f481dc78a7660b90bff5d6214414cb5d0510f3a5
MR BITE <btadamz@gmail.com>
2023-06-06 17:46:22 +0100

    Add files via upload

---
+++  java codes/scripts.js
@@ -0,0 +1,12 @@
+const money = 4
+console.log(money + 3)
+
+function foodProcessor( apples, orangers){
+    console.log(apples, orangers)
+const juice =  'juice with ${apples} apples and ${orangers} oranges;
+return juice;
+}
+const applejuice = foodProcessor(  5, 6)
+console.log(applejuice) 
+love and SVGFESpotLightElement. 
+
